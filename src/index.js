31ddbee0c2df740044d96eea152ea74256622004
Chris Keller <ckeller.tomgeekery@gmail.com>
2018-11-15 21:10:35 -0500

    It moves

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import "phaser";
 import { SimpleScene } from "./scenes/simple-scene";
 
 const gameConfig = {
+  type: Phaser.AUTO,
   width: 680,
   height: 400,
-  scene: SimpleScene
+  scene: SimpleScene,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  }
 };
 
 new Phaser.Game(gameConfig);
--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,10 +1,31 @@
+let player;
+let cursors;
+
 export class  SimpleScene extends Phaser.Scene {
   preload() {
     this.load.image("cokecan", "assets/cokecan.png");
   }
 
   create() {
-    this.add.text(100, 100, "Hello Phaser!", { fill: "#0f0" });
-    this.add.image(100, 200, "cokecan");
+    // Create the player.
+    player = this.physics.add.sprite(100, 200, "cokecan");
+    player.setCollideWorldBounds(true);
+
+    console.log(this.physics);
+
+    // Create cursors.
+    cursors = this.input.keyboard.createCursorKeys();
   }
+
+  update() {
+    if (cursors.right.isDown) {
+      player.setVelocityX(160);
+    }
+    else if (cursors.left.isDown) {
+      player.setVelocityX(-160);
+    }
+    else {
+      player.setVelocityX(0);
+    }
+  };
 }
